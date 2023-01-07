489ef068b53857c9bc6fca459cf60f6292bcf4c5
= <=>
2023-01-07 23:16:31 +0100

    Re-push of previous one

--- appConfiguration.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("APP_ROOT", "/KRUDO");
-
-define("APP_DB_NAME", "krudo");
-define("APP_DB_USER", "root");
-define("APP_DB_PASSWORD", "123321");
-define("APP_DB_PORT", "3309");
-define("APP_DB_ADDRESS", "127.0.0.1");
